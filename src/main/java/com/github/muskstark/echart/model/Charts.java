b5bd2b11f4055dbdc8247a5c82ca6cf63aa1938f
MuskStart <138420547+MuskStark@users.noreply.github.com>
2023-12-26 22:19:17 +0800

    add enum

--- src/main/java/com/github/muskstark/echart/model/Charts.java
+++ src/main/java/com/github/muskstark/echart/model/Charts.java
@@ -2,6 +2,7 @@ package com.github.muskstark.echart.model;
 
 
 import com.github.muskstark.echart.attribute.Title;
+import com.github.muskstark.echart.enums.TypeOfChart;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -11,8 +12,13 @@ public abstract class Charts implements Serializable {
 
     private Title title;
 
-    public void initialize(String type) {
-
-    }
+    /**
+     * 根据输入的图表种类完成图表初始化
+     *
+     * Initializes the chart of the specified type.
+     *
+     * @param  chartType  the type of chart to initialize
+     */
+    public void initialize(TypeOfChart chartType){}
 
 }
