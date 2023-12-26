a864b0b2ac9e0fec8c6e1f992db9264a4be9d789
MuskStart <138420547+MuskStark@users.noreply.github.com>
2023-12-26 22:18:49 +0800

    add: toJson function

--- src/main/java/com/github/muskstark/echart/util/EchartsUtil.java
+++ src/main/java/com/github/muskstark/echart/util/EchartsUtil.java
@@ -1,4 +1,11 @@
 package com.github.muskstark.echart.util;
 
-public class EchartsUtil {
+import com.alibaba.fastjson2.JSON;
+
+public  class EchartsUtil {
+
+    public static String toJson(Object obj) {
+        return JSON.toJSONString(obj);
+    }
+
 }
