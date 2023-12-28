63a7ac737ca6f96c2d89415b2ec3c4bead83817e
MuskStark <138420547+MuskStark@users.noreply.github.com>
2023-12-28 17:45:03 +0800

    add function

--- src/main/java/com/github/muskstark/echart/attribute/Grid.java
+++ src/main/java/com/github/muskstark/echart/attribute/Grid.java
@@ -51,8 +51,63 @@ public class Grid implements Serializable {
         }else {
             throw new RuntimeException(left.getClass() + "不受支持的类型");
         }
-
         return this;
     }
+
+    public Grid right(Object right) {
+        if(right instanceof String || right instanceof Double){
+            this.right = right;
+        }else {
+            throw new RuntimeException(right.getClass() + "不受支持的类型");
+        }
+        return this;
+    }
+
+    public Grid top(Object top) {
+        if(top instanceof String || top instanceof Double){
+            this.top = top;
+        }else {
+            throw new RuntimeException(top.getClass() + "不受支持的类型");
+        }
+        return this;
+    }
+
+    public Grid bottom(Object bottom) {
+        if(bottom instanceof String || bottom instanceof Double){
+            this.bottom = bottom;
+        }else {
+            throw new RuntimeException(bottom.getClass() + "不受支持的类型");
+        }
+        return this;
+    }
+
+    public Grid height(Object height) {
+        if(height instanceof String || height instanceof Double){
+            this.height = height;
+        }else {
+            throw new RuntimeException(height.getClass() + "不受支持的类型");
+        }
+        return this;
+    }
+
+    public Grid width(Object width) {
+        if(width instanceof String || width instanceof Double){
+            this.width = width;
+        }else {
+            throw new RuntimeException(width.getClass() + "不受支持的类型");
+        }
+        return this;
+    }
+
+    public Grid containLabel(Boolean containLabel) {
+        this.containLabel = containLabel;
+        return this;
+    }
+
+    public Grid backgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+        return this;
+    }
+
 
 }
