4b5a3137f1c5826b7220912c24997174c8f69b3a
MuskStark <138420547+MuskStark@users.noreply.github.com>
2023-12-29 10:58:54 +0800

    change test cuz factory class changed

--- src/test/java/com/github/muskstark/echart/model/bar/BarCharTest.java
+++ src/test/java/com/github/muskstark/echart/model/bar/BarCharTest.java
@@ -1,6 +1,6 @@
 package com.github.muskstark.echart.model.bar;
 
-import com.github.muskstark.echart.enums.TypeOfChart;
+import com.github.muskstark.echart.enums.TypeOfBarChart;
 import com.github.muskstark.echart.factory.BarChartFactory;
 import com.github.muskstark.echart.util.EchartsUtil;
 import org.junit.jupiter.api.Test;
@@ -11,16 +11,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class BarCharTest {
 
     @Test
-    public void createBarCharTest() {
-        BarChartFactory factory = new BarChartFactory();
-        BarChar chart = factory.createChart(TypeOfChart.BAR_CHART_BASE);
+    void createNullBarCharTest() {
+
+        BarChar chart = BarChartFactory.createChart(TypeOfBarChart.BAR_CHART);
         System.out.println(EchartsUtil.toJson(chart));
     }
 
     @Test
-    public void defineChartTest() {
-        BarChartFactory factory = new BarChartFactory();
-        BarChar chart = factory.createChart(TypeOfChart.BAR_CHART_BASE);
+    void createBaseBarCharTest() {
+        BarChar chart = BarChartFactory.createChart(TypeOfBarChart.BAR_CHART_BASE);
+        chart.defineXAxis().id("1")
+                .alignTicks(true);
+        chart.defineYAxis().id("2");
+        chart.defineTitle().show(true);
+        System.out.println(EchartsUtil.toJson(chart));
+    }
+
+    @Test
+    void createAlignBarCharTest() {
+        BarChar chart = BarChartFactory.createChart(TypeOfBarChart.BAR_CHART_AXIS_ALIGN_WITH_TICK);
+        System.out.println(EchartsUtil.toJson(chart));
+    }
+
+    @Test
+    void defineChartTest(){
+        BarChar chart = BarChartFactory.createChart(TypeOfBarChart.BAR_CHART);
         chart.defineXAxis().id("1")
                 .alignTicks(true);
         chart.defineYAxis().id("2");
