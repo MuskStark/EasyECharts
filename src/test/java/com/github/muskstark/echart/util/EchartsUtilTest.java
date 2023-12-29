9e34299a42009e062dfcb34dc71e182c1f8f8790
MuskStark <138420547+MuskStark@users.noreply.github.com>
2023-12-29 11:00:43 +0800

    change cuz factory class changed

--- src/test/java/com/github/muskstark/echart/util/EchartsUtilTest.java
+++ src/test/java/com/github/muskstark/echart/util/EchartsUtilTest.java
@@ -1,6 +1,7 @@
 package com.github.muskstark.echart.util;
 
-import com.github.muskstark.echart.enums.TypeOfChart;
+
+import com.github.muskstark.echart.enums.TypeOfBarChart;
 import com.github.muskstark.echart.factory.BarChartFactory;
 import com.github.muskstark.echart.model.bar.BarChar;
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,7 @@ class EchartsUtilTest {
 
     @Test
     void toJson() {
-        BarChartFactory factory = new BarChartFactory();
-        BarChar chart = factory.createChart(TypeOfChart.BAR_CHART);
+        BarChar chart = BarChartFactory.createChart(TypeOfBarChart.BAR_CHART);
         String json = EchartsUtil.toJson(chart);
         System.out.println(json);
     }
