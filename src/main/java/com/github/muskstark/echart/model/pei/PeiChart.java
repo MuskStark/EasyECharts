ee24b89fdc2287fb127f2a2d92f4c1a8df67cbe2
MuskStart <138420547+MuskStark@users.noreply.github.com>
2024-01-02 21:54:22 +0800

    add peichart

---
+++ src/main/java/com/github/muskstark/echart/model/pei/PeiChart.java
@@ -0,0 +1,11 @@
+package com.github.muskstark.echart.model.pei;
+
+import com.github.muskstark.echart.model.Charts;
+import lombok.Getter;
+
+@Getter
+public class PeiChart extends Charts {
+
+
+
+}
