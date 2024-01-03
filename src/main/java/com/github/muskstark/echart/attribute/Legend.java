5f9c9719a49fb8e35aa43d059e175fb81b3313ee
MuskStart <138420547+MuskStark@users.noreply.github.com>
2024-01-03 22:39:44 +0800

    add functions

--- src/main/java/com/github/muskstark/echart/attribute/Legend.java
+++ src/main/java/com/github/muskstark/echart/attribute/Legend.java
@@ -1,5 +1,7 @@
 package com.github.muskstark.echart.attribute;
 
+import com.github.muskstark.echart.enums.EChartsExceptionsEnum;
+import com.github.muskstark.echart.exception.EChartsException;
 import com.github.muskstark.echart.style.line.LineStyle;
 import com.github.muskstark.echart.style.text.TextStyle;
 import lombok.Getter;
@@ -15,7 +17,7 @@ public class Legend {
     private String type;
     private String id;
     private Boolean show;
-    private Double zlevel;
+    private Double zLevel;
     private Double z;
     private Object left;
     private Object top;
@@ -83,5 +85,76 @@ public class Legend {
         this.show = show;
         return this;
     }
+
+    public Legend zLevel(Double zLevel){
+        this.zLevel = zLevel;
+        return this;
+    }
+
+    public Legend z(Double z){
+        this.z = z;
+        return this;
+    }
+
+    public Legend left(Object left){
+        if(left instanceof String || left instanceof Double){
+            this.left = left;
+        }else {
+            throw new EChartsException(EChartsExceptionsEnum.ECharts_Invalid_TypeError);
+        }
+        return this;
+    }
+
+    public Legend top(Object top){
+        if(top instanceof String || top instanceof Double){
+            this.top = top;
+        }else {
+            throw new EChartsException(EChartsExceptionsEnum.ECharts_Invalid_TypeError);
+        }
+        return this;
+    }
+
+    public Legend right(Object right){
+        if(right instanceof String || right instanceof Double){
+            this.right = right;
+        }else {
+            throw new EChartsException(EChartsExceptionsEnum.ECharts_Invalid_TypeError);
+        }
+        return this;
+    }
+
+    public Legend bottom(Object bottom){
+        if(bottom instanceof String || bottom instanceof Double){
+            this.bottom = bottom;
+        }else {
+            throw new EChartsException(EChartsExceptionsEnum.ECharts_Invalid_TypeError);
+        }
+        return this;
+    }
+
+    public Legend width(Object width){
+        if(width instanceof String || width instanceof Double){
+            this.width = width;
+        }else {
+            throw new EChartsException(EChartsExceptionsEnum.ECharts_Invalid_TypeError);
+        }
+        return this;
+    }
+
+    public Legend height(Object height){
+        if(height instanceof String || height instanceof Double){
+            this.height = height;
+        }else {
+            throw new EChartsException(EChartsExceptionsEnum.ECharts_Invalid_TypeError);
+        }
+        return this;
+    }
+
+    public Legend orient(String orient){
+        this.orient = orient;
+        return this;
+    }
+
+
 
 }
